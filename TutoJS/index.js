1b76e1add859432ad9fe82c650c3fe0e56f7e5f0
EmilioFrank007 <50631232+frank245@users.noreply.github.com>
2024-02-16 16:24:46 -0600

    son 4 archivos en total

---
+++ TutoJS/index.js
@@ -0,0 +1,95 @@
+//Esto es solo un comentario
+//console.log("hola Champ");
+
+var miVariable = 24;
+// console.log("mi edad es " + miVariable);
+
+miVariable = 12;
+// console.log("mi edad ahora es " + miVariable);
+
+const miConstante = 3; 
+// console.log("el valor de mi constante es " + miConstante);
+
+//var op1 = 2
+//var op2 = 3
+//var resultado = op1 + op2;
+//console.log("el resultado vale:" + resultado);
+
+// CONDICIONALES 
+
+let miNumero  = 6
+let miNombre = "Emilio";
+
+//let resultadoPregunta = miNumero == 7;
+//console.log(resultadoPregunta);
+
+
+// if(miNumero > 2 && miNombre == "Emilio" ){
+ //   console.log("Si.");
+ // }else {
+ //   console.log("No.");
+// }
+
+//let niNumero2  = 0 
+//if(miNumero2 > 0){
+//    console.log("mi numero es positivo");
+//}else if(miNumero2 === 0){
+//    console.log("mi numero es zero");
+//}else{
+//    console.log("Mi numero es Negativo ");
+//}
+
+//let contador  = 0
+//while(contandor < 5 ){
+//    console.log("Hola a todos")
+//      consoloe.log(contador);
+ //   contador = contandor + 1;
+//}
+
+//for (let i = 1000000; i >= 0; i-- ) {
+ //   console.log(i);
+//}
+
+
+function saludar(nombre, edad){
+    console.log("Hola mi nombre es: " + nombre);
+    console.log("y mi edad es: ") 
+}
+
+
+function multiplicar(num1, num2){
+    let resultado = num1 * num2;
+    return resultado;
+}
+
+//let recibidor = multiplicar(2,5);
+//console.log(recibidor);
+
+//ARREGLOS 
+let miArreglo = [ "David", "Fernando", "Kevin", "Karla"];
+for(let i = 0; i < 4; i++){
+    console.log("Accediendo al indice: "  + i);
+let mostrar = miArreglo[i];
+  console.log(mostrar);
+}
+
+//OBJETOS
+let miObjetivo = {
+    nombre : "david",
+    edad: 24, 
+    masculino : true
+};
+   console.log(persona);
+
+persona.nombre= "Nuevo Leon";
+console.log(persona);
+
+ //let persona2 = {
+ //   nombre: "kevin",
+  //  edad : 21, 
+  //  masculino: false,
+
+// };
+
+// Let arreglodeObjetos = [persona , persona2]
+
